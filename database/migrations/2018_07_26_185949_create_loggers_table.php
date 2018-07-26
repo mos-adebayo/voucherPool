912894f7e0944ddc95f67972be011c769a6137be
moses.adebayo <moses.adebayo@venturegardengroup.com>
2018-07-26 20:06:47 +0100

    Migration for all entities set

---
+++ database/migrations/2018_07_26_185949_create_loggers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLoggersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logger', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('action');
+            $table->text('description');
+            $table->dateTime('createdAt');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logger');
+    }
+}
